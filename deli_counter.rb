bb9347dccf71534a595540ca4bf75a18a25cc667
Emily Lillie <emilylillie3@gmail.com>
2019-07-17 22:00:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,4 +11,4 @@ def take_a_number(katz_deli, name)
    if katz_deli.length == 0 
      puts "There is nobody waiting to be served!"
    else
-     puts ""
+     puts "Currently serving #{katz_deli.shift}"
